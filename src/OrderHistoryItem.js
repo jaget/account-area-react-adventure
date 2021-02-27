53a1b955bf4e66bec2cfb79a4e787c9e64607342
jaget <juliantaylor14@googlemail.com>
2021-02-27 11:47:19 +0000

    Start basic implementation. Fetch data and load in basic components

--- src/OrderHistoryItem.js
+++ src/OrderHistoryItem.js
@@ -1,8 +1,120 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Dispatch from './OrderHistoryItem/Dispatch';
+import OrderNumber from './OrderHistoryItem/OrderNumber';
+import OrderType from './OrderHistoryItem/OrderType';
+import Price from './OrderHistoryItem/Price';
+import ProductCard from './OrderHistoryItem/ProductCard';
+import DeliveryDetails from './OrderHistoryItem/DeliveryDetails';
 
 const OrderHistoryItem = () => {
+  const [orderHistory, setOrderHistory] = useState({});
+  const [dataLoaded, setDataLoaded] = useState(false); //Whether the data was loaded successfully from API endpoint
+  const [dataLoading, setDataLoading] = useState(true); //Whether we are waiting for a response from API endpoint
+
+  useEffect(() => {
+    /**
+     * I should use axios or some other suitable library.
+     * However I'm not to familiar with it.
+     * Fetch does the job 9/10 times.
+     */
+
+    fetch('https://reactasty.apps.huel.io/api/customer/orders', {
+      method: 'get'
+    })
+      .then(res => res.ok && res.json())
+      .then(res => {
+        setDataLoading(false);
+
+        const formattedResponse = res[0]; // Response is an array rather than an object so we want first element in that array.
+        if (formattedResponse.success) {
+          setDataLoaded(true);
+          setOrderHistory(formattedResponse.orders);
+        } else {
+          setDataLoaded(false);
+        }
+      });
+  }, []);
+
   return (
     <div>
+      {!dataLoading && !dataLoaded && (
+        <div className="column is-12">
+          An error occurred loading your order history. Please try again later
+          or{' '}
+          <a href="/pretend-support-page">Contact our (pretend) support team</a>
+        </div>
+      )}
+
+      {dataLoading && (
+        <div className="column is-12">Loading order history please wait</div>
+      )}
+
+      {!dataLoading && dataLoaded && (
+        <div className="column is-12">
+          <div className="box orders-history-block has-shadow-hover">
+            <div className="is-flex orders-block-header">
+              <OrderNumber orderNumber="467614-US" />
+              <OrderType orderType="One-time" />
+              <Price price="113.86" />
+              <Dispatch dispatchDate="August 7th 2019" />
+            </div>
+
+            <hr />
+
+            <div className="order-information">
+              <p className="title is-6 is-marginless">
+                It&apos;s been dispatched!
+              </p>
+
+              <div>
+                <div className="order-information-expanded">
+                  <div className="product-list-boxes columns is-multiline">
+                    <ProductCard
+                      imageAlt="Product bars"
+                      imageSrc="https://huel-assets.s3.eu-west-2.amazonaws.com/temp-public/thumbnails/powder.jpg"
+                      productTitle="Huel Powder"
+                      productVariant={[
+                        '1x Berry',
+                        '1x Vanilla',
+                        '1x Chocolate'
+                      ]}
+                      price={99.0}
+                    />
+                    <div className="column is-6">
+                      <div className="media">
+                        <div className="media-left">
+                          <img
+                            alt="Product bars"
+                            className="image"
+                            src="https://cdn.shopify.com/s/files/1/0578/1097/products/HUEL_SHAKER_FROSTER_FR_1200.jpg?v=1515319444"
+                          />
+                        </div>
+                        <div className="media-content">
+                          <div>
+                            <p className="product-title">
+                              Huel Shaker Bottle (Clear)
+                            </p>
+                            <p className="product-variants">
+                              1x Huel Shaker Bottle (Clear)
+                            </p>
+                          </div>
+                        </div>
+                        <div className="media-right">
+                          <p className="product-price">$5.00</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <DeliveryDetails deliveryAddress="925 N La Brea Ave, West Hollywood, 90038" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="column is-12">
         <div className="box orders-history-block has-shadow-hover">
           <div className="is-flex orders-block-header">
